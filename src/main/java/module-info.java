54af0b52b1a1e561733e549eea63c81db6de7ad5
embet-mengistu <131568421+embet-mengistu@users.noreply.github.com>
2025-05-28 19:42:12 -0700

    i

---
+++ src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module org.example.sanwichapp_gui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+
+    opens org.example.sanwichapp_gui to javafx.fxml;
+    exports org.example.sanwichapp_gui;
+}
